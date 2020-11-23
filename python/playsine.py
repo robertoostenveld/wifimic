01ec8d4504d1ba3875c671ceaa4014a9be4a370c
Robert Oostenveld <r.oostenveld@gmail.com>
2020-11-23 22:24:21 +0100

    fixed order of code and parameters

--- python/playsine.py
+++ python/playsine.py
@@ -5,16 +5,16 @@ import numpy as np
 
 p = pyaudio.PyAudio()
 
+volume = 1.0     # range [0.0, 1.0]
+fs = 22050       # sampling rate, Hz, must be integer
+duration = 1.0   # in seconds, may be float
+f = 440.0        # sine frequency, Hz, may be float
+
 # for paFloat32 sample values must be in range [-1.0, 1.0]
 stream = p.open(format=pyaudio.paInt16,
                 channels=1,
                 rate=fs,
                 output=True)
-
-volume = 1.0     # range [0.0, 1.0]
-fs = 22050       # sampling rate, Hz, must be integer
-duration = 1.0   # in seconds, may be float
-f = 440.0        # sine frequency, Hz, may be float
 
 samples = volume * 32767. * (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)+1)/2
 samples = samples.astype(np.uint16)
