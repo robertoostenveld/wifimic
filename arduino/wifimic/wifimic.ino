4e2f34fa87f3dcf2c2dc2e822352e29363c16ff5
Robert Oostenveld <r.oostenveld@gmail.com>
2021-04-04 14:47:26 +0200

    move to an interrupt driven handling, using 4 pages

--- arduino/wifimic/wifimic.ino
+++ arduino/wifimic/wifimic.ino
@@ -20,20 +20,20 @@
 #include "g711.h"
 
 // see https://en.wikipedia.org/wiki/Real-time_Transport_Protocol, one of these should be defined
-#define RTP_ULAW
+// #define RTP_ULAW
 // #define RTP_ALAW
-// #define RTP_L16
+#define RTP_L16
 
 #define LED_BUILTIN 22
 
-#define USE_WIFI        // required for TCP and UDP
+#define USE_WIFI       // required for TCP and UDP
 #define USE_UDP
 // #define USE_TCP
-// #define DO_RECONNECT // not needed for UDP
+// #define DO_RECONNECT   // not needed for UDP
 // #define DO_FILTER
 // #define DO_LIMITER
 // #define DO_THRESHOLD
-#define DO_CLOCKSYNC
+// #define DO_CLOCKSYNC
 // #define PRINT_VALUE
 // #define PRINT_RANGE
 // #define PRINT_VOLUME
@@ -76,17 +76,18 @@ const unsigned char rtpType = 11;
 const unsigned int sampleRate = 44100;
 #endif
 
-const unsigned int nMessage = 400;      // this can be up to 720 and still fit within the MTU of 1500 bytes
-const unsigned int nBuffer = 1024;  // minimum 8, maximum 1024
-unsigned int samplesReady = 0;
+const unsigned int nPage = 4;
+const unsigned int nPacket = 400;      // the number of samples in a network packet can be up to 720 and still fit within the MTU of 1500 bytes
+const unsigned int nBuffer = 1024;     // the number of samples in the I2S buffer is minimum 8, maximum 1024
 long timestampOffset = 0;
 float timestampSlope = 1;
 bool meanInitialized = 0;
 const double alpha = 10. / sampleRate;  // if the sampling time dT is much smaller than the time constant T, then alpha=1/(T*sampleRate) and T=1/(alpha*sampleRate)
 double signalMean = sqrt(-1);           // initialize as not-a-number
-const double dbMax = 90.3;              // this is the loudest that an int16 signal can get
+const double dbMax = 90.3;              // this is the loudest that an int16 signal can get, alternating between MININT16 and MAXINT16
 const double volumeThreshold = -40;     // relative to the maxiumum
-const double signalDivider = pow(2, 12);
+const double signalDivider = pow(2, 11);
+
 /*
     With a divider of 2^16=65536 blowing hard into the mic still does not clips. In this case the limiter is not needed.
     With a divider of 2^13=8192 normal speech does not clips. In this case the limiter is not needed.
@@ -102,7 +103,7 @@ unsigned long getTimestamp() {
 }
 
 // see https://en.wikipedia.org/wiki/Real-time_Transport_Protocol
-struct message_t {
+struct {
   // the format of the packet header is always the same
   uint8_t version = (2 << 6);  // the RTP version is 2
   uint8_t type = rtpType;      // the RTP type is 0, 1, or 11
@@ -112,19 +113,21 @@ struct message_t {
   // the data is represented according to one of these
 #if defined(RTP_ULAW)
   // type=0  PCMU  audio 1 8000  any 20  ITU-T G.711 PCM μ-Law audio 64 kbit/s               RFC 3551
-  uint8_t data[nMessage];
+  uint8_t data[nPacket];
 #elif defined(RTP_ALAW)
   // type=8  PCMA  audio 1 8000  any 20  ITU-T G.711 PCM A-Law audio 64 kbit/s               RFC 3551
-  uint8_t data[nMessage];
+  uint8_t data[nPacket];
 #elif defined(RTP_L16)
   // type=11 L16   audio 1 44100 any 20  Linear PCM 16-bit audio 705.6 kbit/s, uncompressed  RFC 3551, Page 27
-  int16_t data[nMessage];
+  int16_t data[nPacket];
 #endif
-} message __attribute__((packed));
+} packet __attribute__((packed));
 
-esp_err_t err;
-uint32_t bytes_read;
-int32_t buffer[nBuffer];
+int32_t *page[nPage];
+int32_t samples[nPage];
+uint8_t filling = 0;
+uint8_t sending = 0;
+
 const i2s_port_t I2S_PORT = I2S_NUM_0;
 
 RunningStat shortStat;
@@ -141,13 +144,13 @@ void WiFiEvent(WiFiEvent_t event) {
       Udp.begin(WiFi.localIP(), udpPort);
 #endif
       // use the last number of the IP address the mesage identifier
-      message.id = WiFi.localIP()[3];
+      packet.id = WiFi.localIP()[3];
       wifiConnected = true;
       break;
     case SYSTEM_EVENT_STA_DISCONNECTED:
       Serial.println("WiFi lost connection.");
       // do not use the last number of the IP address as identifier any more
-      message.id = 0;
+      packet.id = 0;
       wifiConnected = false;
 #ifdef USE_UDP
       Udp.stop();
@@ -176,6 +179,13 @@ void SyncHandler(AsyncUDPPacket packet) {
 }
 #endif
 
+hw_timer_t * timer = NULL;
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+
+void IRAM_ATTR onTimer() {
+}
+
+
 /**************************************************************************************************/
 
 void setup() {
@@ -185,6 +195,11 @@ void setup() {
 
   pinMode(32, OUTPUT); digitalWrite(32, HIGH);  // L/R
   pinMode(35, OUTPUT); digitalWrite(35, HIGH);  // VDD
+
+  for (int i = 0; i < nPage; i++) {
+    page[i] = (int32_t *) malloc(nPacket * sizeof(int32_t));
+    samples[i] = 0;
+  }
 
   const i2s_pin_config_t pin_config = {
     .bck_io_num = 25,                   // Serial Clock (SCK on the INMP441)
@@ -262,9 +277,10 @@ void setup() {
   }
   Serial.println("I2S pins set.");
 
-  for (int i; i < 2 * sampleRate; i++) {
-    err = i2s_read(I2S_PORT, buffer, 4, &bytes_read, 0);
-  }
+  timer = timerBegin(0, 80, true);     // use a divider of 80 to make the timer count in microseconds
+  timerAttachInterrupt(timer, &onTimer, true);
+  timerAlarmWrite(timer, 10000, true); // run 100x per second
+  timerAlarmEnable(timer);
 
 } // setup
 
@@ -272,27 +288,31 @@ void setup() {
 
 void loop() {
 
-  if ((millis() - lastBlink) > 2000) {
-    digitalWrite(LED_BUILTIN, LOW);
+  if ((millis() - lastBlink) > blinkInterval / 2) {
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
     lastBlink = millis();
   }
-  else if ((millis() - lastBlink) > 1000) {
-    digitalWrite(LED_BUILTIN, HIGH);
-  }
 
-  // how many audio samples do we still need to fill the next message?
-  size_t samplesNeeded = nMessage - samplesReady;
-  if (samplesNeeded > nBuffer) {
+  // how many samples do we still need to fill the current page?
+  size_t samplesNeeded = nPacket - samples[filling];
+
+  if (samplesNeeded > nBuffer)
     // do not read more than nBuffer at a time
     samplesNeeded = nBuffer;
-  }
 
-  err = i2s_read(I2S_PORT, buffer, samplesNeeded * 4, &bytes_read, 0);
+  uint32_t bytes_read;
+  esp_err_t err = i2s_read(I2S_PORT, page[filling] + samples[filling], samplesNeeded * 4, &bytes_read, 0);
+  if (err == ESP_OK)
+    samples[filling] += bytes_read / 4;
 
-  if (err == ESP_OK) {
-    for (unsigned int sample; sample < bytes_read / 4; sample++) {
+  if (samples[filling] == nPacket)
+    // the current page is full, move on to the next page
+    filling = (filling + 1) % nPage;
 
-      double value = buffer[sample];
+  if (samples[sending] == nPacket) {
+
+    for (unsigned int sample; sample < nPacket; sample++) {
+      double value = page[sending][sample];
 
       // compute a smooth running mean with https://en.wikipedia.org/wiki/Exponential_smoothing
       if (isnan(signalMean)) {
@@ -320,120 +340,121 @@ void loop() {
 #endif
 
 #if defined(RTP_ULAW)
-      message.data[samplesReady] = linear2ulaw(value); // this casts the value to an int16, and then converts to uint8
+      packet.data[sample] = linear2ulaw(value); // this casts the value to an int16, and then converts to uint8
 #elif defined(RTP_ALAW)
-      message.data[samplesReady] = linear2alaw(value); // this casts the value to an int16, and then converts to uint8
+      packet.data[sample] = linear2alaw(value); // this casts the value to an int16, and then converts to uint8
 #elif defined(RTP_L16)
-      message.data[samplesReady] = value; // this casts the value to an int16
+      packet.data[sample] = value; // this casts the value to an int16
 #endif
 
-      samplesReady++;
       shortStat.Push(value);
     }
 
-    if (samplesReady == nMessage) {
-
 #ifdef PRINT_RANGE
-      Serial.print(shortstat.Min());
-      Serial.print(", ");
-      Serial.print(shortstat.Mean());
-      Serial.print(", ");
-      Serial.println(shortstat.Max());
+    Serial.print(shortstat.Min());
+    Serial.print(", ");
+    Serial.print(shortstat.Mean());
+    Serial.print(", ");
+    Serial.println(shortstat.Max());
 #endif
 
 #ifdef PRINT_FREQUENCY
-      Serial.println(shortstat.ZeroCrossingRate() * sampleRate);
+    Serial.println(shortstat.ZeroCrossingRate() * sampleRate);
 #endif
 
 #ifdef PRINT_VOLUME
-      Serial.println(10 * log10(shortstat.Variance()) - dbMax);
+    Serial.println(10 * log10(shortStat.Variance()) - dbMax);
 #endif
 
 #ifdef PRINT_HEADER
-      Serial.print(message.version); Serial.print(", ");
-      Serial.print(message.type); Serial.print(", ");
-      Serial.print(message.counter); Serial.print(", ");
-      Serial.print(message.timestamp); Serial.print(", ");
-      Serial.println(message.id);
+    Serial.print(packet.version); Serial.print(", ");
+    Serial.print(packet.type); Serial.print(", ");
+    Serial.print(packet.counter); Serial.print(", ");
+    Serial.print(packet.timestamp); Serial.print(", ");
+    Serial.println(packet.id);
 #endif
 
 #ifdef PRINT_ELAPSED
-      thisPacket = millis();
-      Serial.println(thisPacket - lastPacket);
-      lastPacket = thisPacket;
+    thisPacket = millis();
+    Serial.println(thisPacket - lastPacket);
+    lastPacket = thisPacket;
 #endif
 
 #ifdef DO_RECONNECT
-      if  (lastConnect == 0 || (millis() - lastConnect) > maintenanceInterval) {
-        // reconnect to TCP, but don't try to reconnect too often
-        lastConnect = millis();
+    if  (lastConnect == 0 || (millis() - lastConnect) > maintenanceInterval) {
+      // reconnect to TCP, but don't try to reconnect too often
+      lastConnect = millis();
 
-        // turn the status LED on
-        digitalWrite(LED_BUILTIN, LOW);
-        lastBlink = millis();
+      // turn the status LED on
+      digitalWrite(LED_BUILTIN, LOW);
+      lastBlink = millis();
 
-        if (wifiConnected && !tcpConnected) {
-          tcpConnected = Tcp.connect(serverAddress, tcpPort);
-          if (tcpConnected) {
-            Serial.print("Connected to ");
-            Serial.println(serverAddress);
-          }
-          else {
-            Serial.print("Failed to connect to ");
-            Serial.println(serverAddress);
-          }
+      if (wifiConnected && !tcpConnected) {
+        tcpConnected = Tcp.connect(serverAddress, tcpPort);
+        if (tcpConnected) {
+          Serial.print("Connected to ");
+          Serial.println(serverAddress);
+        }
+        else {
+          Serial.print("Failed to connect to ");
+          Serial.println(serverAddress);
         }
       }
+    }
 #endif
 
-      if (wifiConnected) {
-        blinkInterval = 1000;
+    if (wifiConnected) {
+      blinkInterval = 1000;
 
 #ifdef DO_THRESHOLD
-        bool aboveThreshold = (10 * log10(shortstat.Variance() - dbMax) > volumeThreshold);
-        if (aboveThreshold)
-          lastThreshold = millis();
-        else if ((millis() - lastThreshold) < thresholdInterval)
-          aboveThreshold = 1;
+      bool aboveThreshold = (10 * log10(shortstat.Variance() - dbMax) > volumeThreshold);
+      if (aboveThreshold)
+        lastThreshold = millis();
+      else if ((millis() - lastThreshold) < thresholdInterval)
+        aboveThreshold = 1;
 #else
-        bool aboveThreshold = 1;
+      bool aboveThreshold = 1;
 #endif
 
-        if (aboveThreshold) {
-          int count;
+      if (aboveThreshold) {
+        int count;
 #ifdef USE_UDP
-          Udp.beginPacket(serverAddress, udpPort);
-          count = Udp.write((uint8_t *)(&message), sizeof(message));
-          Udp.endPacket();
-          udpConnected = (count == sizeof(message));
-          if (!udpConnected) {
-            Serial.println("not connected to udp");
-            blinkInterval = 250;
-          }
+        Udp.beginPacket(serverAddress, udpPort);
+        count = Udp.write((uint8_t *)(&packet), sizeof(packet));
+        Udp.endPacket();
+        udpConnected = (count == sizeof(packet));
+        if (!udpConnected) {
+          Serial.println("not connected to udp");
+          blinkInterval = 250;
+        }
 #endif
 #ifdef USE_TCP
-          count = Tcp.write((uint8_t *)(&message), sizeof(message));
-          tcpConnected = (count == sizeof(message));
-          if (!tcpConnected) {
-            Serial.println("not connected to tcp");
-            blinkInterval = 250;
-          }
+        count = Tcp.write((uint8_t *)(&packet), sizeof(packet));
+        tcpConnected = (count == sizeof(packet));
+        if (!tcpConnected) {
+          Serial.println("not connected to tcp");
+          blinkInterval = 250;
+        }
 #endif
-        } // if aboveThreshold
-      } // if wifiConnected
+      } // if aboveThreshold
+    } // if wifiConnected
 
-      else {
+    else {
 #ifdef USE_WIFI
-        Serial.println("not connected to wifi");
+      Serial.println("not connected to wifi");
 #endif
-        blinkInterval = 250;
-      }
-
-      shortStat.Clear();
-      samplesReady = 0;
-      message.counter++;
-      message.timestamp = getTimestamp();
+      blinkInterval = 250;
     }
+
+    shortStat.Clear();
+    packet.counter++;
+    packet.timestamp = getTimestamp();
+
+    // flag this page as empty
+    samples[sending] = 0;
+
+    // move on to the next page
+    sending = (sending + 1) % nPage;
   }
 
 
